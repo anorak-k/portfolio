a4461a846f3db55698813c4e89f36d72c833372e
Maadesh Sivakumar <maadesh.sivakumar@gmail.com>
2024-02-06 00:35:39 -0500

    Fix. Contact image, remove footer image, remove subscription box

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 // import contactImg from "../assets/img/contact-img.svg";
-import contactImg from "../assets/img/skills-NYU.png";
+import contactImg from "../assets/img/nyu-brown.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 
