c4fb1b4df7c011da3ec60e2d1521388413a02bb7
Maadesh Sivakumar <maadesh.sivakumar@gmail.com>
2024-02-11 23:43:39 -0500

    Add. Project titles to the cards

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ProjectCard from './ProjectCard'
 
 const projectData = [
-  { title: "Project 1", description: "This project aim to whatever sdjfnjksd", color: "#DCE775" },
-  { title: "Project 2", description: "This project aim to whatever sdjfnjksd", color: "#4DD0E1" },
-  { title: "Project 3", description: "This project aim to whatever sdjfnjksd", color: "#FF8A65" },
-  { title: "Project 4", description: "This project aim to whatever sdjfnjksd", color: "#9575CD" },
-  { title: "Project 5", description: "This project aim to whatever sdjfnjksd", color: "#3755db" },
+  { title: "Modified ResNet architecture on CIFAR 10", description: "This project aim to whatever sdjfnjksd", color: "#DCE775" },
+  { title: "Modern Asteroids game in Java", description: "This project aim to whatever sdjfnjksd", color: "#4DD0E1" },
+  { title: "Predicting patients with higher risk of dying with COVID-19", description: "This project aim to whatever sdjfnjksd", color: "#FF8A65" },
+  { title: "Remote Health Monitoring System for the Elderly", description: "This project aim to whatever sdjfnjksd", color: "#9575CD" },
+  { title: "Analyzing DeepLearning approaches for Spoiler Detection in Text", description: "This project aim to whatever sdjfnjksd", color: "#3755db" },
 ];
-
 
 
 
@@ -16,7 +15,7 @@ export const Projects = () => {
   return (
     <div className="projects-section">
       <div className="projects-container">
-        <h1>My Projects</h1>
+        <h1>Some of my latest projects</h1>
         {projectData.map((project, index) => (
           <ProjectCard key={index} project={project} />
         ))}
